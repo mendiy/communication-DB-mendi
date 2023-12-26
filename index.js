c51537828f72408548166727d6fd1a9fea818a5b
mendi <mendiby@gmail.com>
2023-12-26 16:17:44 +0200

    update file

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ connectDB();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: ["GET", "POST"],
   },
 });
